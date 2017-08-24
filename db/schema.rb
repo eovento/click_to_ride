b245d83b1e09cce6baf8ea70a461db26989c3ba2
Daniel Ribeiro <asthedayislong@gmail.com>
2017-08-24 16:57:52 -0300

    Cloudinary + Attachinary Working. Model Car created and relations established

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170816150648) do
+ActiveRecord::Schema.define(version: 20170824193841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attachinary_files", force: :cascade do |t|
+    t.string   "attachinariable_type"
+    t.integer  "attachinariable_id"
+    t.string   "scope"
+    t.string   "public_id"
+    t.string   "version"
+    t.integer  "width"
+    t.integer  "height"
+    t.string   "format"
+    t.string   "resource_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["attachinariable_type", "attachinariable_id", "scope"], name: "by_scoped_parent", using: :btree
+  end
+
+  create_table "cars", force: :cascade do |t|
+    t.string   "brand"
+    t.string   "model"
+    t.integer  "year"
+    t.text     "description"
+    t.string   "document"
+    t.string   "plate"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_cars_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -38,8 +66,10 @@ ActiveRecord::Schema.define(version: 20170816150648) do
     t.string   "location"
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "cars", "users"
 end
